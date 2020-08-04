0bc3a8072c710621c5ea52ce3ef614b5630e24af
leomccormack <leo.t.mccormack@gmail.com>
2020-08-04 15:51:54 +0300

    more work on the mex wrappers

--- extras/mex/BUILD_SAF_MEX.m
+++ extras/mex/BUILD_SAF_MEX.m
@@ -1,15 +1,56 @@
+%
+% Copyright 2020 Leo McCormack
+%
+% Permission to use, copy, modify, and/or distribute this software for any
+% purpose with or without fee is hereby granted, provided that the above
+% copyright notice and this permission notice appear in all copies.
+%
+% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+% REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+% AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+% INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+% LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+% OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+% PERFORMANCE OF THIS SOFTWARE.
+%
 clear all %#ok
+
+% 1. compile SAF via CMake prior to running this script
+% 2. configure user options below as needed
+% If you encounter any problems during compilation, please refer to the FAQ 
+% at the bottom of this script before submitting a bug report.
 
 
 %% User Options 
-saf_perf_lib = 'SAF_USE_INTEL_MKL'; % 'SAF_USE_INTEL_MKL', 'SAF_USE_APPLE_ACCELERATE', 'SAF_USE_OPEN_BLAS_AND_LAPACKE'
-run_tests = 1; % 0:disable, 1:enable
-cmake_build_folder = '../../build'; % absolute/relative path to CMake build folder (compile saf prior to running script)
+saf_perf_lib = []; % leave empty to scan cache, 'SAF_USE_INTEL_MKL', 'SAF_USE_APPLE_ACCELERATE', 'SAF_USE_OPEN_BLAS_AND_LAPACKE'
+run_tests = 1;     % 0: disabled, 1: enabled
+cmake_build_folder = '../../build/'; % path to CMake build folder
+outputFolder = 'mex_output/';
+
+
+%% Pull settings from CMakeCache.txt if not defined in user options 
+if isempty(saf_perf_lib)
+    text = fileread([cmake_build_folder 'CMakeCache.txt']);
+    txt = textscan(text,'%s','delimiter','\n'); 
+    cac = [txt{:}];
+
+    idx =[];
+    if isempty(idx)
+        idx = find(strcmp( cac, 'SAF_PERFORMANCE_LIB:UNINITIALIZED=SAF_USE_INTEL_MKL' ));
+        if ~isempty(idx), saf_perf_lib = 'SAF_USE_INTEL_MKL'; end 
+    elseif isempty(idx)
+        idx = find(strcmp( cac, 'SAF_PERFORMANCE_LIB:UNINITIALIZED=SAF_USE_APPLE_ACCELERATE' ));
+        if ~isempty(idx), saf_perf_lib = 'SAF_USE_APPLE_ACCELERATE'; end 
+    elseif isempty(idx)
+        idx = find(strcmp( cac, 'SAF_PERFORMANCE_LIB:UNINITIALIZED=SAF_USE_OPEN_BLAS_AND_LAPACKE' ));
+        if ~isempty(idx), saf_perf_lib = 'SAF_USE_OPEN_BLAS_AND_LAPACKE'; end 
+    end  
+end
 
 
 %% Configure
 header_paths = {'../../framework/include'};
-lib_paths = {[ cmake_build_folder '/framework' ]};
+lib_paths = {[ cmake_build_folder 'framework' ]};
 libs = {};
 defines = {saf_perf_lib};
 
@@ -18,7 +59,7 @@ if ismac
     libs{end+1} = 'saf';
     
     % Error checking
-    if ~exist([cmake_build_folder '/framework/libsaf.a'], 'file'), error('libsaf.a is missing, run CMake and build saf first'), end
+    if ~exist([cmake_build_folder 'framework/libsaf.a'], 'file'), error('libsaf.a is missing, run CMake and build saf first'), end
     
     % extra header+lib paths
     switch saf_perf_lib
@@ -30,29 +71,54 @@ if ismac
             libs{end+1} = 'saf_mkl_custom';
             saf_mkl = [lib_paths{end} '/lib' libs{end} '.dylib' ];
             if ~exist(saf_mkl, 'file'), error([ saf_mkl ' is missing']), end
-    
     end 
+    
 elseif isunix
     assert(0); %currently unsupported 
 elseif ispc
     assert(0); %currently unsupported 
 end
 
-% prepends
+% prepends and stack
 header_paths = cellfun(@(c)['-I' c],header_paths,'uni',false);
 lib_paths = cellfun(@(c)['-L' c],lib_paths,'uni',false);
 libs = cellfun(@(c)['-l' c],libs,'uni',false);
 defines = cellfun(@(c)['-D' c],defines,'uni',false);
+compilerFlags = {header_paths{:} lib_paths{:} libs{:} defines{:}}; %#ok
+
+% output folder
+if ~exist(outputFolder, 'dir'), mkdir(outputFolder); end
 
 
 %% Build MEX
-mex('-v', header_paths{:}, lib_paths{:}, libs{:}, defines{:}, 'saf_getSHreal.c');
+mex('-v', compilerFlags{:}, './src/saf_getSHreal.c', '-outdir', outputFolder);
+mex('-v', compilerFlags{:}, './src/saf_getSHcomplex.c', '-outdir', outputFolder);
 
 
 %% TESTS
-A = ones(10,1).';
-s = 2;
-B = saf_getSHreal(s,A) 
+if ~run_tests, return; end
+addpath(outputFolder)
+nTests = 0; nPass = 0; nFail = 0;
+
+% saf_getSHreal
+order = 5;
+dirs = randn(1000,2); 
+Y = saf_getSHreal(order,dirs); 
+Y_ref = getSH(order, dirs, 'real').'; 
+nTests = nTests+1;
+if max(abs(Y(:)-Y_ref(:)))<1e-6, nPass=nPass+1; else, nFail=nFail+1; end 
+
+% saf_getSHcomplex
+order = 4;
+dirs = randn(400,2); 
+Y = saf_getSHcomplex(order,dirs);
+Y_ref = getSH(order, dirs, 'complex').';
+nTests = nTests+1;
+if max(abs(Y(:)-Y_ref(:)))<1e-6, nPass=nPass+1; else, nFail=nFail+1; end 
+
+% Output
+display(['Number of tests: ' num2str(nTests)]);
+display(['Number of failures: ' num2str(nFail)]);
 
 
 %% FAQ
