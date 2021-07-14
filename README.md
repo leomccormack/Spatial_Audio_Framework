6c0a7f285897e716a2b040f50f93a8f0d95a2070
leomccormack <leo.t.mccormack@gmail.com>
2021-07-14 18:28:54 +0300

    switched mkl/ipp to use the oneAPI by default also for MSVC

--- README.md
+++ README.md
@@ -89,13 +89,13 @@ target_link_libraries(${PROJECT_NAME} PRIVATE saf)
 
 The available SAF-related CMake options (and their default values) are:
 ```
--DSAF_PERFORMANCE_LIB=SAF_USE_INTEL_MKL    # performance library to employ
--DSAF_ENABLE_SOFA_READER_MODULE=0          # enable/disable the saf_sofa_reader module 
--DSAF_ENABLE_TRACKER_MODULE=0              # enable/disable the saf_tracker module 
--DSAF_BUILD_EXAMPLES=1                     # build saf examples
--DSAF_BUILD_EXTRAS=0                       # build safmex etc.
--DSAF_BUILD_TESTS=1                        # build unit testing program
--DSAF_USE_INTEL_IPP=0                      # To link and use Intel IPP for the FFT etc.
+-DSAF_PERFORMANCE_LIB=SAF_USE_INTEL_MKL_LP64 # performance library to employ
+-DSAF_ENABLE_SOFA_READER_MODULE=0            # enable/disable the saf_sofa_reader module 
+-DSAF_ENABLE_TRACKER_MODULE=0                # enable/disable the saf_tracker module 
+-DSAF_BUILD_EXAMPLES=1                       # build saf examples
+-DSAF_BUILD_EXTRAS=0                         # build safmex etc.
+-DSAF_BUILD_TESTS=1                          # build unit testing program
+-DSAF_USE_INTEL_IPP=0                        # To link and use Intel IPP for the FFT, resampler, etc.
 ```
 
 If using e.g. **SAF_USE_INTEL_MKL_LP64** as the performance library, note that the default header and library search paths may be overridden [according to your setup](docs/PERFORMANCE_LIBRARY_INSTRUCTIONS.md) with:
--- scripts/install-safmkl.bat
+++ scripts/install-safmkl.bat
@@ -9,12 +9,12 @@ ECHO ***************************************************************************
 ECHO ************** Custom MKL library installer for the Spatial_Audio_Framework **************
 ECHO ******************************************************************************************
 ECHO.
-ECHO This batch script will build the required saf_mkl_custom.lib and saf_mkl_custom.dll files
+ECHO This batch script will build the required saf_mkl_custom and saf_mkl_custom files
 ECHO and copy them into:
-ECHO   - "Spatial_Audio_Framework/dependencies/Win64/lib/saf_mkl_custom.lib" 
-ECHO   - "C:/Windows/System32/saf_mkl_custom.dll".  
-ECHO You may choose between sequential and threaded versions of the library. The latter option
-ECHO will also place libiomp5md.lib and libiomp5md.dll, in these same folders.
+ECHO   - "Spatial_Audio_Framework/dependencies/Win64/lib/saf_mkl_custom_[lp64|ilp64].lib" 
+ECHO   - "C:/Windows/System32/saf_mkl_custom[lp64|ilp64].dll".  
+ECHO You may choose between sequential and threaded versions of the library. Sequential is the
+ECHO recommended option.
 ECHO.
 ECHO NOTE: you will need to run this script using the "x64 Command Prompt for VS.exe", which 
 ECHO will require Administrator privileges!
@@ -22,15 +22,14 @@ ECHO.
 
 :: ========================================================================= ::
 :: Check that MKL is installed
-IF NOT EXIST "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder" (
+IF NOT EXIST "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder" (
     echo Intel MKL not installed on this machine. Note that Intel MKL can be freely downloaded from here:
-    echo "https://software.intel.com/en-us/articles/free-ipsxe-tools-and-libraries"
+    echo "https://software.intel.com/content/www/us/en/develop/tools/oneapi/base-toolkit/download.html"
     EXIT /B
 )
 :: Copy saf_mkl_list to MKL builder folder
 echo Copying saf_mkl_list into MKL builder folder
-xcopy "saf_mkl_list" "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder"
-
+xcopy "saf_mkl_list" "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder"
 
 :: ========================================================================= ::
 ::CLS
@@ -60,29 +59,25 @@ IF ERRORLEVEL 1 GOTO Parallel_LP64
 :Parallel_LP64
 ECHO.
 ECHO Building threaded versions of the custom MKL library (LP64 interface) for SAF...
-cd C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/tools/builder
+cd C:/Program Files (x86)/Intel/oneAPI/mkl/latest/tools/builder
 nmake intel64 interface=lp64 threading=parallel name=saf_mkl_custom_lp64 export=saf_mkl_list
 ECHO.
 ECHO Copying files to correct folders...
 cd /d "%~dp0"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_lp64.lib" "..\dependencies\Win64\lib" 
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_lp64.dll" "C:\Windows\System32\"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64\libiomp5md.lib" "..\dependencies\Win64\lib"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\redist\intel64\compiler\libiomp5md.dll" "C:\Windows\System32\"
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_lp64.lib" "..\dependencies\Win64\lib" 
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_lp64.dll" "C:\Windows\System32\"
 GOTO End
 
 :Parallel_ILP64
 ECHO.
 ECHO Building threaded versions of the custom MKL library (ILP64 interface) for SAF...
-cd C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/tools/builder
+cd C:/Program Files (x86)/Intel/oneAPI/mkl/latest/tools/builder
 nmake intel64 interface=ilp64 threading=parallel name=saf_mkl_custom_ilp64 export=saf_mkl_list
 ECHO.
 ECHO Copying files to correct folders...
 cd /d "%~dp0"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_ilp64.lib" "..\dependencies\Win64\lib" 
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_ilp64.dll" "C:\Windows\System32\"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64\libiomp5md.lib" "..\dependencies\Win64\lib"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\redist\intel64\compiler\libiomp5md.dll" "C:\Windows\System32\"
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_ilp64.lib" "..\dependencies\Win64\lib" 
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_ilp64.dll" "C:\Windows\System32\"
 GOTO End
 
 :Sequential
@@ -99,25 +94,25 @@ IF ERRORLEVEL 1 GOTO Sequential_LP64
 :Sequential_LP64
 ECHO.
 ECHO Building sequential versions of the custom MKL library (LP64 interface) for SAF...
-cd C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/tools/builder
+cd C:/Program Files (x86)/Intel/oneAPI/mkl/latest/tools/builder
 nmake intel64 interface=lp64 threading=sequential name=saf_mkl_custom_lp64 export=saf_mkl_list
 ECHO.
 ECHO Copying files to correct folders...
 cd /d "%~dp0"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_lp64.lib" "..\dependencies\Win64\lib" 
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_lp64.dll" "C:\Windows\System32\"
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_lp64.lib" "..\dependencies\Win64\lib" 
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_lp64.dll" "C:\Windows\System32\"
 GOTO End
 
 :Sequential_ILP64
 ECHO.
 ECHO Building sequential versions of the custom MKL library (ILP64 interface) for SAF...
-cd C:/Program Files (x86)/IntelSWTools/compilers_and_libraries/windows/mkl/tools/builder
+cd C:/Program Files (x86)/Intel/oneAPI/mkl/latest/tools/builder
 nmake intel64 interface=ilp64 threading=sequential name=saf_mkl_custom_ilp64 export=saf_mkl_list
 ECHO.
 ECHO Copying files to correct folders...
 cd /d "%~dp0"
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_ilp64.lib" "..\dependencies\Win64\lib" 
-xcopy "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl\tools\builder\saf_mkl_custom_ilp64.dll" "C:\Windows\System32\"
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_ilp64.lib" "..\dependencies\Win64\lib" 
+xcopy "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\tools\builder\saf_mkl_custom_ilp64.dll" "C:\Windows\System32\"
 GOTO End
 
 :End
